1ea05efbbb8448683ea09c8109dcfa953becf8d9
arrorobbi <54522683+arrorobbi@users.noreply.github.com>
2019-12-06 19:00:58 +0700

    Update FINAL PROJECT.sh

--- Legacy Codes/FINAL PROJECT.sh
+++ Legacy Codes/FINAL PROJECT.sh
@@ -1,224 +1,225 @@
 clear
-i=0
-declare -a nama
-declare -a kode
-declare -a lahir
-declare -a mati
-declare -a agama
+z=0
+menu=0
 
-cetak()
-{
-	echo -e "Data yang telah dimasukkan : \n"			
-		for (( q=0; q<i;q++  ))
-		do
-			echo -e "Data ke $[q+1]\nNama Mayat            : ${nama[q]}\nNomor Kematian        : ${kode[q]}\nTempat, tanggal lahir : ${lahir[q]}\nTempat, tanggal wafat : ${mati[q]}\nAgama                 : ${agama[q]}\n"
-		done
+input(){
+	  
+    let z=$z+1
+	echo "Menu : "
+	echo "1.Nasi Geprek 	 Rp.10,000"
+	echo "2.Lontong Balap  Rp.10,000"
+	echo "3.Lapis Daging 	 Rp.12,000"
+	echo "4.Es Teh	 Rp.4,000"
+	echo "5.Indomie Goreng Rp.4,500"
+	echo "6.Indomie Kuah 	 Rp.5,000"
+	echo "======================="
+	echo -n "Nama Pelangan : "
+	read pelanggan[$z]
+	echo -n "Nama Kasir : "
+	read kasir[$z]
+	echo -n "Tanggal :"
+	read tanggal[$z]
+	echo -n "Harga menu 1 : "
+	read menu1[$z]
+	echo -n "Harga menu 2 : "
+	read menu2[$z]
+	let total=${menu1[$z]}+${menu2[$z]}
+	echo "TOTAL $total "
+	echo -n "Masukkan Total Harga :"
+	read totalharga[$z]
+	echo -n "No. Resi : "
+	read noreg[$z]
+           
 }
 
-while :;
+
+mutasitagihan(){
+
+	   for((a=1;a<=z;a++))
+	do
+		echo ""
+		echo ""
+		echo "Mutasi Tagihan"
+		echo "Tagihan a/n ${pelanggan[$a]}"
+		echo "No.Resi ${noreg[$a]}"
+		echo "Tanggal ${tanggal[$a]}"
+		echo "Total Harga ${totalharga[$a]}"
+	done
+}
+
+search(){
+	    echo -n "Masukkan No Resi     : "
+            read cari
+            a=0
+            b=0
+            while [ $a -le $z ] && [ $b == 0 ]
+		do
+                        let a=$a+1
+                        if [ "${noreg[a]}" == $cari ]
+                        then
+				b=1
+                        fi
+            done
+}
+
+delete(){
+	echo "HAPUS DATA TRANSAKSI"
+	echo "Jika Salah Memilih Menu Tekan Enter"
+	echo "==================================="
+	    search
+	    if [ $a -gt $z ]
+            	then
+                        echo "Data Tidak Ada"
+            else
+                        while [ $a -lt $z ]
+                        	do
+                                    let b=$a+1
+				    pelanggan[$a]=${pelanggan[$b]}
+				    kasir[$a]=${kasir[$b]}
+				    menu1[$a]=${menu1[$b]}
+                                    menu2[$a]=${menu2[$b]}
+                                    totalharga[$a]=${totalharga[$b]}
+				    resi[$a]=${noreg[$b]}
+                                    let a=$a+1
+                        done
+                        let z=$z-1
+                        echo "Data Transaksi $cari Berhasil Dihapus"
+            fi
+}
+
+
+datapemasukan(){
+for((a=1;a<=z;a++))
+	do
+		echo ""
+		echo ""
+		echo "- - -DATA PEMBELIAN $a- - -"
+		echo "Nama Pelangan 		: ${pelanggan[$a]}"
+		echo "Nama Kasir 		: ${kasir[$a]}"
+		echo "Tanggal 		: ${tanggal[$a]}"
+		echo "Harga menu 1 		: ${menu1[$a]}"
+		echo "Harga menu 2	 	: ${menu2[$a]}"
+		echo "Total Harga		: ${totalharga[$a]}"
+		echo "No. Resi 		: ${noreg[$a]}"
+	done
+}
+
+
+
+update(){
+	echo "UPDATE DATA TRANSAKSI"
+	echo "Jika Salah Memilih Menu Tekan Enter"
+	echo "==================================="
+	    search
+	    if [ $a -le $z ]
+            	then
+			echo "- - -DATA PEMBELIAN $a- - -"
+			echo "Nama Pelangan 		: ${pelanggan[$a]}"
+			echo "Nama Kasir 		: ${kasir[$a]}"
+			echo "Tanggal 		: ${tanggal[$a]}"
+			echo "Harga menu 1 		: ${menu1[$a]}"
+			echo "Harga menu 2	 	: ${menu2[$a]}"
+			echo "Total Harga		: ${totalharga[$a]}"
+			echo "No. Resi 		: ${noreg[$a]}"
+                        echo "- - --- - -"
+			echo -n "Nama Pelangan : "
+			read pelanggan[$z]
+			echo -n "Nama Kasir : "
+			read kasir[$z]
+			echo -n "Tanggal :"
+			read tanggal[$z]
+			echo -n "Harga. menu 1 : "
+			read menu1[$z]
+			echo -n "Harga menu 2 : "
+			read menu2[$z]
+			let total=${menu1[$z]}+${menu2[$z]}
+			echo "TOTAL $total "
+			echo -n "Masukkan Total Harga :"
+			read totalharga[$z]
+			echo -n "No. Resi : "
+			read noreg[$z]
+			echo "=============================================================="
+                        echo "Harga Menu 1				: Rp ${menu1[$z]}"
+                        echo "Harga Menu 2				: Rp ${menu2[$z]}"
+			echo "====	Total Yang Harus Anda Bayar	: Rp ${totalharga[$z]}"
+			echo "=============================================================="
+            else
+                        echo "Data Tidak Ada"
+            fi
+           
+}
+
+
+cetak(){
+	    search
+            if [ $a -le $z ]
+            	then
+			echo ""
+		echo ""
+		echo "- - -DATA TRANSAKSI $a- - -"
+		echo "Nama Pelangan 		: ${pelanggan[$a]}"
+		echo "Nama Kasir 		: ${kasir[$a]}"
+		echo "Tanggal 		: ${tanggal[$a]}"
+		echo "No. Resi 		: ${noreg[$a]}"
+		echo "=============================================================="
+                echo "Harga Menu 1				: Rp ${menu1[$a]}"
+                echo "Harga Menu 2				: Rp ${menu2[$a]}"
+		echo "====	Total Yang Harus Anda Bayar	: Rp ${totalharga[$a]}"
+		echo "=============================================================="
+            else
+                        echo "Data Tidak Ada!!"
+            fi
+}
+
+
+while [ $menu != 7 ]
 do
-	echo -e "Sistem Informasi Data Mayat pada Tempat Pemakaman Umum\n\n1. Masukkan data mayat\n2. Lihat data mayat\n3. Cari data mayat\n4. Perbarui data mayat\n5. Hapus data mayat\n6. Keluar program\n"
- 	echo -n "Masukkan pilihan : "
- 	read pilih
-
- 	if (("$pilih" == 1));   # INPUT DATA
- 	then 
-  		clear
- 	 	echo -n "Nama Mayat            : "
- 	 	read nama[$i]
- 	 	echo -n "Nomor Kematian        : "
-  		read kode[$i]
-	  	echo -n "Tempat, tanggal lahir : "
-  		read lahir[$i]
-		echo -n "Tempat, tanggal wafat : "
-  		read mati[$i]
-	  	echo -n "Agama                 : "
-  		read agama[$i]
- 	 	i=`expr $i + 1`
-		
-		if (("$i" >= 2))
-		then
-			for (( g=0; g<i-1; g++ ))
-			do
-	   			if (( "${kode[$i-1]}" == "${kode[g]}" ))
+	echo "#	**KASIR KANTIN 86**		#"
+  	echo ""
+        echo "1.Input Transaksi"
+        echo "2.Mutasi Pembelian"
+	echo "3.Search"
+        echo "4.Data Transaksi"
+	echo "5.Delete Informasi Kasir"
+	echo "6.Update Informasi Kasir"
+	echo "7.Exit"
+        echo -n "Masukkan Pilihan Anda : "
+            read menu
+            if [ $menu -eq 1 ]
+            	then
+		clear
+                        input  
+            elif [ $menu -eq 2 ]
+            	then
+		clear
+			if [ $z -lt 1 ]
 				then
-		     			echo -e "\nNomor kematian ini sudah terdaftar!\nMasukan nomor yang lain!"
-					read
-					unset nama[$i-1]
-					unset kode[$i-1]
-					unset lahir[$i-1]
-					unset mati[$i-1]
-					unset agama[$i-1]
-					i=`expr $i - 1`
-					break
-	   			fi
-			done
-		fi
- 	 	clear
- 
- 	elif (("$pilih" == 2));   # TAMPILKAN DATA
- 	then
-  		if (( i == 0 ))
-  		then
-   			clear
-   			echo "Tidak ada data yang dapat ditampilkan"
-   			read
-   			clear
-  		else
-   			clear
-   			cetak
-   			read
-   			clear
-  		fi
-
- 	elif (("$pilih" == 3));   # CARI DATA
- 	then
-  		if (( i == 0))
-  		then
-   			clear
-   			echo "Tidak ada data yang dapat dicari"
-   			read
-   			clear
-  		else  
-   			clear
-      			echo -n "Masukkan nomor kematian yang ingin dicari : "
-      			read cari
-   
-      			k=0
-      			while (($cari != ${kode[$k]}))
-      			do
-    				k=`expr $k + 1`
-      			done
-      
-   			if (($cari == ${kode[$k]}));
-   			then
-    				echo -e "Nama Mayat            : ${nama[k]}\nNomor Kematian        : ${kode[k]}\nTempat, tanggal lahir : ${lahir[k]}\nTempat, tanggal wafat : ${mati[k]}\nAgama                 : ${agama[k]}\n"   
-
-   			else
-    				clear    
-    				echo -e "Data tidak ditemukan"
-   			fi
-   			read
-      			clear
-  		fi
-   
- 	elif (("$pilih" != 4 && "$pilih" != 2 && "$pilih" != 1 && "$pilih" != 3 && "$pilih" != 5 && "$pilih" != 6));   # APABILA MEMBERIKAN MASUKAN SELAIN 1-6
- 	then
-  		echo "Pilihan tidak valid"
-  		read
-  		clear
-
-	elif (( "$pilih" == 5 ));   # HAPUS DATA
-	then
-		if (( i == 0))
-  		then
-   			clear
-   			echo "Tidak ada data yang dapat dihapus"
-   			read
-   			clear
-  		else  
-   			clear
-   			cetak
-			echo -n "Masukkan nomor kematian : "
-      			read cari
-   
-      			k=0
-      			while (($cari != ${kode[$k]}))
-      			do
-    				k=`expr $k + 1`
-      			done
-      
-   			if (($cari == ${kode[$k]}));
-   			then			
-    				unset nama[k]
-				unset kode[k]
-				unset lahir[k]
-				unset mati[k]
-				unset agama[k]
-
-				if [[ -z ${kode[k]} ]]
-				then
-					if (($i == 1))
-					then
-						i=0
-	
-					elif (($i == 2))
-					then
-						for ((f=$k; f<i; f++))
-						do
-							nama[f]=${nama[$f+1]}
-							kode[f]=${kode[$f+1]}
-							lahir[f]=${lahir[$f+1]}
-							mati[f]=${mati[$f+1]}
-							agama[f]=${agama[$f+1]}
-						done
-						i=`expr $i - 1`
-
-					elif (($i > 2))
-					then
-						if [[ -n ${kode[$k-1]} ]]
-						then
-							for (( d=$k; d<i; d++ ))
-							do
-								nama[d]=${nama[$d+1]}
-								kode[d]=${kode[$d+1]}
-								lahir[d]=${lahir[$d+1]}
-								mati[d]=${mati[$d+1]}
-								agama[d]=${agama[$d+1]}
-							done
-						fi
-						i=`expr $i - 1`
-					fi									
-				fi
-   			else
-    				clear    
-    				echo -e "Data tidak ditemukan"
-   			fi
-   			read
-      			clear
-  		fi
-
-	elif (("$pilih" == 4));   # MEMPERBAHARUI DATA
-	then
-		if (( i == 0))
-  		then
-   			clear
-   			echo "Tidak ada data yang dapat diperbarui"
-   			read
-   			clear
-  		else  
-   			clear
+                                    echo "kosong "
+                	else
+                                    mutasitagihan
+                	fi
+	    elif [ $menu -eq 3 ]
+	    	then
+		clear
 			cetak
-      			echo -e -n "\nMasukkan nomor kematian yang ingin diperbarui : "
-      			read baru
-   
-      			l=0
-      			while (($baru != ${kode[$l]}))
-      			do
-    				l=`expr $l + 1`
-      			done
-      
-   			if (($baru == ${kode[$l]}));
-   			then
-				echo -n "Nama Mayat            : "
-		 	 	read nama[$l]
-			  	echo -n "Tempat, tanggal lahir : "
-		  		read lahir[$l]
-				echo -n "Tempat, tanggal wafat : "
-		  		read mati[$l]
-			  	echo -n "Agama                 : "
-		  		read agama[$l]
-
- 	 			clear
-   			else
-    				clear    
-    				echo -e "Data tidak ditemukan"
-				read
-   			fi
-      			clear
-  		fi
- 
- 	else    # KELUAR PROGAM
-  		exit
- 	fi
+            elif [ $menu -eq 4 ]
+            	then
+		clear
+                        datapemasukan
+            elif [ $menu -eq 5 ]
+            	then
+		clear
+                        delete
+            elif [ $menu -eq 6 ]
+            	then
+		clear
+                        update
+            elif [ $menu -eq 7 ]
+            	then
+		clear
+                        echo "	**Terimakasih Telah Menggunakan Layanan Kami :)**	"          
+            else
+                        echo "Menu Yang Anda Inputkan Salah!"
+            fi
+            echo ""
 done
